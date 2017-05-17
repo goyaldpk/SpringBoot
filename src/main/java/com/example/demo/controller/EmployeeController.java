d74de2b2fe2481a5d3e11b0d9d917887e304bd11
deepak <goyaldpk68@gmail.com>
2017-05-17 14:05:05 +0530

    This is my first commit
    Changes in controller reagrding cleaning up the code.

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,9 +1,5 @@
 package com.example.demo.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.websocket.server.PathParam;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,9 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.google.gson.Gson;
+
 import com.example.demo.modal.Employee;
  
 @RestController
@@ -23,10 +17,9 @@ public class EmployeeController
     @RequestMapping(value = "/addEmployee", method= RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public Employee addEmployees() 
     {
-    	/*List<Employee> employeesList = new ArrayList<Employee>();
-        */
+    	
     	Employee em = new Employee(1,"lokesh","malhotra","lokesh@gmail.com");
-       // Gson gson = new Gson(employeesList);
+      
         return em;
     }
     
