f7085f1cfbe1dbcede03e913691fa04ea021b4f2
deepak <deepakg@triconinfotech.com>
2017-05-17 15:44:05 +0530

    Commiting Gson changes

--- src/test/java/com/example/demo/SpringStarterApplicationTests.java
+++ src/test/java/com/example/demo/SpringStarterApplicationTests.java
@@ -4,6 +4,7 @@ import static com.jayway.restassured.RestAssured.get;
 import com.jayway.restassured.RestAssured;
 import static com.jayway.restassured.RestAssured.given;
 import static com.jayway.restassured.RestAssured.when;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 
 import org.junit.Before;
@@ -13,7 +14,6 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Value;
 
 
@@ -34,9 +34,16 @@ public class SpringStarterApplicationTests {
 	    }
 
 	    @Test
-	    public void addEmployee() {
+	    public void addEmployeeTest() {
 	        when().get("/addEmployee").
 	                then().statusCode(200).body("id",equalTo(1));
+	    }
+	    
+	    @Test
+	    public void greetingsTest(){
+	    	given().contentType("application/json").
+			when().get("/greetings/Rajan").
+			then().body("greetings", equalTo("Welcome Rajan"));
 	    }
 
 }
